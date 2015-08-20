5f99564eeab2ff1321f49249a2674c98822ceff5
Stepan Kholodov <holodovsya@gmail.com>
2015-08-20 13:26:58 +0300

    -graphite=- option handling added

--- statsdaemon.go
+++ statsdaemon.go
@@ -71,6 +71,8 @@ var (
 	receiveCounter  = flag.String("receive-counter", "", "Metric name for total metrics recevied per interval")
 	prefix           = flag.String("prefix", "stats", "Prefix of reported metrics")
 	percentThreshold = Percentiles{}
+	openTSDBAddress  = flag.String("opentsdb", "127.0.0.1:2003", "openTSDB service address (or - to disable)")
+
 )
 
 func init() {
@@ -133,50 +135,50 @@ func submit(deadline time.Time) error {
 	var num int64
 
 	now := time.Now().Unix()
-
-	client, err := net.Dial("tcp", *graphiteAddress)
-	if err != nil {
-		if *debug {
-			log.Printf("WARNING: resetting counters when in debug mode")
-			processCounters(&buffer, now)
-			processGauges(&buffer, now)
-			processTimers(&buffer, now, percentThreshold)
-		}
-		errmsg := fmt.Sprintf("dialing %s failed - %s", *graphiteAddress, err)
-		return errors.New(errmsg)
-	}
-	defer client.Close()
-
-	err = client.SetDeadline(deadline)
-	if err != nil {
-		errmsg := fmt.Sprintf("could not set deadline:", err)
-		return errors.New(errmsg)
-	}
-
-	num += processCounters(&buffer, now)
-	num += processGauges(&buffer, now)
-	num += processTimers(&buffer, now, percentThreshold)
-	if num == 0 {
-		return nil
-	}
-
-	if *debug {
-		for _, line := range bytes.Split(buffer.Bytes(), []byte("\n")) {
-			if len(line) == 0 {
-				continue
+	if *graphiteAddress!="-"{
+		client, err := net.Dial("tcp", *graphiteAddress)
+		if err != nil {
+			if *debug {
+				log.Printf("WARNING: resetting counters when in debug mode")
+				processCounters(&buffer, now)
+				processGauges(&buffer, now)
+				processTimers(&buffer, now, percentThreshold)
 			}
-			log.Printf("DEBUG: %s", line)
+			errmsg := fmt.Sprintf("dialing %s failed - %s", *graphiteAddress, err)
+			return errors.New(errmsg)
 		}
+		defer client.Close()
+	
+		err = client.SetDeadline(deadline)
+		if err != nil {
+			errmsg := fmt.Sprintf("could not set deadline:", err)
+			return errors.New(errmsg)
+		}
+	
+		num += processCounters(&buffer, now)
+		num += processGauges(&buffer, now)
+		num += processTimers(&buffer, now, percentThreshold)
+		if num == 0 {
+			return nil
+		}
+	
+		if *debug {
+			for _, line := range bytes.Split(buffer.Bytes(), []byte("\n")) {
+				if len(line) == 0 {
+					continue
+				}
+				log.Printf("DEBUG: %s", line)
+			}
+		}
+
+		_, err = client.Write(buffer.Bytes())
+		if err != nil {
+			errmsg := fmt.Sprintf("failed to write stats - %s", err)
+			return errors.New(errmsg)
+		}
+
+		log.Printf("sent %d stats to %s", num, *graphiteAddress)
 	}
-
-	_, err = client.Write(buffer.Bytes())
-	if err != nil {
-		errmsg := fmt.Sprintf("failed to write stats - %s", err)
-		return errors.New(errmsg)
-	}
-
-	log.Printf("sent %d stats to %s", num, *graphiteAddress)
-
 	return nil
 }
 
@@ -357,7 +359,6 @@ func main() {
 		fmt.Printf("statsdaemon v%s (built w/%s)\n", VERSION, runtime.Version())
 		return
 	}
-
 	signalchan = make(chan os.Signal, 1)
 	signal.Notify(signalchan, syscall.SIGTERM)
 	*persistCountKeys = -1 * (*persistCountKeys)
